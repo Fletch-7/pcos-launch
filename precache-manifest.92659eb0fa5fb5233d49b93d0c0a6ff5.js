64e02d6045e08bf94c5111d789ce0143473be1f0
Fletch-7 <r.a.m.fletcher7@gmail.com>
2020-05-11 20:17:55 +0100

    Updates

---
+++ precache-manifest.92659eb0fa5fb5233d49b93d0c0a6ff5.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "380e483fd8766b1e9662929dd7d28dee",
+    "url": "/pcos-launch/index.html"
+  },
+  {
+    "revision": "ed8e93454e70b9c2bfc0",
+    "url": "/pcos-launch/static/css/2.c4f291ab.chunk.css"
+  },
+  {
+    "revision": "20a2ebf65ad76bbd53ce",
+    "url": "/pcos-launch/static/css/main.342a9333.chunk.css"
+  },
+  {
+    "revision": "ed8e93454e70b9c2bfc0",
+    "url": "/pcos-launch/static/js/2.5b75f7c1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/pcos-launch/static/js/2.5b75f7c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20a2ebf65ad76bbd53ce",
+    "url": "/pcos-launch/static/js/main.a627119b.chunk.js"
+  },
+  {
+    "revision": "01d53012de46649edd49",
+    "url": "/pcos-launch/static/js/runtime-main.0209c068.js"
+  },
+  {
+    "revision": "cd0b45258a60713b9ea72360da98df2a",
+    "url": "/pcos-launch/static/media/gym-1.cd0b4525.JPG"
+  },
+  {
+    "revision": "8cdea6f4ba75e17e38378ce516525cf7",
+    "url": "/pcos-launch/static/media/gym-2.8cdea6f4.JPG"
+  },
+  {
+    "revision": "1412220c2f558cdc404be0355294db87",
+    "url": "/pcos-launch/static/media/gym-3.1412220c.JPG"
+  },
+  {
+    "revision": "58ded299fe6916d5bedbe8562dfe7ee5",
+    "url": "/pcos-launch/static/media/gym-5.58ded299.JPG"
+  },
+  {
+    "revision": "cfee5f7742d7daac84373bf63de01e36",
+    "url": "/pcos-launch/static/media/gym-6.cfee5f77.JPG"
+  },
+  {
+    "revision": "e632da93df167e45d497931aaf626667",
+    "url": "/pcos-launch/static/media/gym-7.e632da93.JPG"
+  },
+  {
+    "revision": "9882e569baeab3b6e10fd3698b6ab045",
+    "url": "/pcos-launch/static/media/profile-2.9882e569.JPG"
+  },
+  {
+    "revision": "e5110ac9b0c9a139452a2341eeff2c61",
+    "url": "/pcos-launch/static/media/transform-1.e5110ac9.JPG"
+  },
+  {
+    "revision": "9705fad516d1b20099741a201c2aa2c0",
+    "url": "/pcos-launch/static/media/transform-2.9705fad5.JPG"
+  },
+  {
+    "revision": "6c75f78ba4eaf68b7974eb7d8229c768",
+    "url": "/pcos-launch/static/media/transform-3.6c75f78b.JPG"
+  }
+]);

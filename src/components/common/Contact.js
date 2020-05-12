779fee7f98cda8a3078405019035c751774b4727
Fletch-7 <r.a.m.fletcher7@gmail.com>
2020-05-12 19:11:15 +0100

    respoinse-14

--- src/components/common/Contact.js
+++ src/components/common/Contact.js
@@ -2,6 +2,8 @@ import React from 'react'
 
 import P1 from '../assets/gym-1.JPG'
 import P2 from '../assets/profile-2.JPG'
+import P3 from '../assets/beach.JPG'
+import P4 from '../assets/beach-2.JPG'
 
 class Contact extends React.Component{
   render(){
@@ -14,6 +16,11 @@ class Contact extends React.Component{
         <p>I want to reiterate how you are not alone and I am here to help. I want to help cut the corners for you and teach you everything I know and have experienced. That way you can be on the right path to living your best life! </p>
         <br/>
         <p>Want to know prices and set up a FREE consultation call? Get in contact today!</p>
+        <br/>
+        <div className="contact-container">
+        <img src={P3} id="profile" alt="pic" className="is-flex-mobile"/>
+        <img src={P4} id="profile" alt="pic" className="is-hidden-mobile"/>
+        </div>
         <br/>
         <a href="mailto:itsapcosparty@gmail.com" className="button" alt="link"><i className="far fa-envelope"></i><span>itsapcosparty@gmail.com</span></a>
         <a href="tel:+44 7949 836 939" className="button" alt="link"><i className="fas fa-phone"></i><span>Call Me : +44 7949 836 939</span></a>
